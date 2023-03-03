6f32beac140b22336ddff8ed102e2e673d224c4a
japhethbear <japhethbear@gmail.com>
2023-03-03 15:58:30 -0500

    add axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Pokemon from './components/Pokemon'
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
@@ -8,11 +9,17 @@ function App() {
 
   useEffect(() => {
   
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
-          .then(response => response.json())
-          .then(response => setPokemon(response.results))
-          .catch(err => console.log(err));
+    // fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+    //       .then(response => response.json())
+    //       .then(response => setPokemon(response.results))
+    //       .catch(err => console.log(err));
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+    .then(response=> {
+      setPokemon(response.data.results)})
+    .catch(err => console.log(err))
   }, []);
+
+
 
   return (
     <div className="App">
